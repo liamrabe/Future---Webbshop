66dc31bb3e1b31986e3341ff884dbca2e7dc904c
Liam Rabe <liamrabe@hotmail.com>
2020-02-26 15:12:47 +0100

    Created navbar + banner

--- index.php
+++ index.php
@@ -7,8 +7,35 @@
 	<meta charset="UTF-8">
 </head>
 <body>
-
-	
-
+	<div class="navbar">
+		<div class="navbar-wrapper">
+			<div class="left">
+				<a href="/home" class="nav-link">Home</a>
+				<a href="/about" class="nav-link">About us</a>
+				<a href="/products" class="nav-link">Product</a>
+				<a href="/blog" class="nav-link">Blog</a>
+				<a href="/contact" class="nav-link">Contact</a>
+				<a href="/guestbook" class="nav-link">Guest Book</a>
+			</div>
+			<div class="right">
+				<a href="/register" class="nav-link">Create account</a>
+				<a href="/login" class="nav-link">Sign in</a>
+			</div>
+		</div>
+	</div>
+	<div class="banner">
+		<div class="banner-container">
+			<div class="banner-wrapper">
+				<div class="banner-title">Dream On</div>
+				<div class="banner-paragraph">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Nulla mattis nisi sed neque malesuada, ac malesuada magna
+					sodales. Vestibulum commodo faucibus blandit. Suspendisse
+					a erat non erat pretium accumsan non vel purus. Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit.
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
