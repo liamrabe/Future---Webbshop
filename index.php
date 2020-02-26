5aad7df5a8da69bdb45ef49a6303aced11287180
Liam Rabe <liamrabe@hotmail.com>
2020-02-26 13:51:09 +0100

    Intializing...

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="https://liam.se/css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Viggo Slutuppgift - Home</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+
+	
+
+</body>
+</html>
---
+++ src/cl.bat
@@ -0,0 +1,6 @@
+SET watch="E:/projekt/php/www/viggo-slutuppgift/src/less"
+SET destination="E:/projekt/php/www/viggo-slutuppgift/static/css"
+SET main="main.less"
+
+cls
+less-watch-compiler %watch% %destination% %main%
