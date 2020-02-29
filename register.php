7bb44eae19b93ee7ca235e8af4e13df3acc5b3ff
Liam Rabe <liamrabe@hotmail.com>
2020-02-29 16:51:40 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php
+	require_once "./lib/database.php";
+	$db = new Database();
+
+	include "./partials/html_begin.html";
+	
+?>
+
+<div class="form-body">
+	<div class="form-container">
+		<form action="/login" method="post" class="form register">
+			
+		</form>
+	</div>
+</div>
+
+
+<?php
+	include "./partials/html_end.html";
+?>
