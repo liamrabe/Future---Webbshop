fea09ddc27c8eae71bb260fdba922af7897c9f65
Liam Rabe <liamrabe@hotmail.com>
2020-02-29 17:19:34 +0100

    Created and fixed register form.

--- register.php
+++ register.php
@@ -3,18 +3,24 @@
 	$db = new Database();
 
 	include "./partials/html_begin.html";
+	include "./partials/navbar.php";
 	
 ?>
 
 <div class="form-body">
 	<div class="form-container">
-		<form action="/login" method="post" class="form register">
-			
+		<form action="/register" method="post" class="form register">
+			<h1 class="form-title">Bli medlem</h1>
+			<input type="text" placeholder="Username" name="username" class="input" id="username" required>
+			<input type="password" placeholder="Password" name="password" class="input" id="password" required>
+			<input type="email" placeholder="Email" name="email" class="input" id="email" required>
+			<button class="button submit">Bli medlem</button>
 		</form>
 	</div>
 </div>
 
 
 <?php
+	include "./partials/footer.php";
 	include "./partials/html_end.html";
 ?>
