e5909ffe8697dce31f9aca0ee17778e6f432784a
Liam Rabe <liamrabe@hotmail.com>
2020-03-08 01:53:21 +0100

    Minor revision

--- signout.php
+++ signout.php
@@ -1,9 +1,6 @@
 <?php
 
-	require $_SERVER["DOCUMENT_ROOT"] . "/lib/database.php";
-	$db = new Database();
-
-	$db->destroycookie("access_token");
+	setcookie("access_token", null, 1, "/", $_SERVER["SERVER_NAME"], true, false);
 	session_destroy();
 
 	header("location: /home");
