c103c37e6f4eaca363e4259e796998126f833037
liamrabe <liamrabe@hotmail.com>
2022-05-09 17:54:06 +0200

    Deleted version 1.0

--- partials/html_end.php
+++
@@ -1,3 +0,0 @@
-	<script src="https://<?= $_SERVER["SERVER_NAME"]; ?>/js/navbar.js"></script>
-</body>
-</html>
--- src/cl.bat
+++
@@ -1,6 +0,0 @@
-SET watch="E:/projekt/php/www/viggo-slutuppgift/src/less"
-SET destination="E:/projekt/php/www/viggo-slutuppgift/static/css"
-SET main="main.less"
-
-cls
-less-watch-compiler %watch% %destination% %main%
